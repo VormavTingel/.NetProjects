d83d6c4fee41a1ddf7273f23e73a95a7cdbbf796
VormavTingel <gabrielhenrique.musico@gmail.com>
2025-03-02 00:38:33 -0300

    commit 1

--- TaskManager/TaskManagerAPI/Model/TaskItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TaskManagerAPI.Models
-{
-    public class TaskItem
-    {
-        public int Id { get; set; }           // Chave primária
-        public string Title { get; set; } = string.Empty;
-        public bool IsCompleted { get; set; }
-    }
-}
